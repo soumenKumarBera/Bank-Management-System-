21cc3abfcc0c963db91b2543d924983720ef0fdd
soumen kumar bera <soumenbera4017@gmail.com>
2026-02-27 12:00:26 +0530

    create customer details

--- bank-application/src/Main.java
+++ bank-application/src/Main.java
@@ -1,15 +1,77 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import service.BankService;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        BankService services = new BankService();
+        int choice;
+        do{
+
+            System.out.println("=================================");
+            System.out.println("\t\t\tKB Bank");
+            System.out.println("=================================");
+            System.out.println("Welcome!\nChoose one of the following options : ");
+            System.out.println("1. Create new bank account");
+            System.out.println("2. Close account");
+            System.out.println("3. Withdraw funds");
+            System.out.println("4. Deposit funds");
+            System.out.println("5. Transfer funds to another account");
+            System.out.println("6. View transaction history");
+            System.out.println("7. View bank account details");
+            System.out.println("8. Update account holder's details");
+            System.out.println("9. EXIT");
+            System.out.println("=================================");
+            System.out.print("Enter your choice : ");
+            choice = sc.nextInt();
+           switch (choice){
+               case 1:
+                   System.out.println("For creating a bank account, please provide the following details of bank account holder: ");
+                   System.out.print("First name : ");
+                   String fname = sc.nextLine();
+                    sc.nextLine();
+                   System.out.print("Last name : ");
+                   String lname = sc.nextLine();
+
+                   System.out.print("email-id : ");
+                   String email = sc.nextLine();
+
+                   System.out.print("Phone number : ");
+                   String  phoneNumber= sc.nextLine();
+
+                   System.out.print("Address : ");
+                   String address = sc.nextLine();
+
+                   System.out.print("Aadhar number : ");
+                   long adharNumber = sc.nextLong();
+                   sc.nextLine();
+                   System.out.print("PAN number : ");
+                   String panNumber = sc.nextLine();
+
+                   services.createAccount(fname, lname, email, phoneNumber,  address, adharNumber, panNumber);
+                   break;
+               case 2:
+               case 3:
+               case 4:
+               case 5:
+               case 6:
+               case 7:
+               case 8:
+               case 9:break;
+               default: System.out.println("Invalid option selected.\nKindly choose a valid option again.");
+                break;
+
+
+           }
+
+
+
+        } while(choice != 9);
+
     }
+
 }
--- bank-application/src/service/BankService.java
+++ bank-application/src/service/BankService.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class BankService {
 
 
-
+    public void createAccount(String fname, String lname, String email, String phoneNumber, String address, long adharNumber, String panNumber) {
+    }
 }
